17439623f64cf9b66a7a14ad4b67c1e39177f9ed
harshgoel05 <harshgoel05@gmail.com>
2020-02-29 16:13:37 +0530

    minor

--- js/script-main.js
+++ js/script-main.js
@@ -1,31 +1,32 @@
-
 AOS.init({
   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
+  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: "aos-init", // class applied after initialization
+  animatedClassName: "aos-animate", // class applied on animation
   useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 350, // values from 0 to 3000, with step 50ms
-  easing: 'linear', // default easing for AOS animations
+  easing: "linear", // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
   mirror: true, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
+  anchorPlacement: "top-bottom" // defines which position of the element regarding to window should trigger the animation
 });
-$(document).ready(function () {
-    $('.navbar-toggler').on('click', function () {
-      $('.hamburger-animated').toggleClass('open');
-    });
+$(document).ready(function() {
+  $(".navbar-toggler").on("click", function() {
+    $(".hamburger-animated").toggleClass("open");
+  });
 });
 
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();   
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+$(".navbar-nav>li>a").on("click", function() {
+  $(".navbar-collapse").collapse("hide");
+  $(".hamburger-animated").toggleClass("open");
 });
